11088a2fc5ef3c3d02df491c3510a1aa2306f221
hao.tao <hao.tao@ttpai.cn>
2024-08-16 22:53:42 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import AutoImport from 'unplugin-auto-import/vite'
+
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    uni(),
+    AutoImport({
+      imports: ["vue", "uni-app", "pinia"],
+      dts: true,
+     })
+  ],
+})
